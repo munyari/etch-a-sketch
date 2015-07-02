ca7a751e94e7f29fd7c15148ba0fcf49a1615ef0
Panashe Fundira <fundirap@gmail.com>
2015-07-01 21:41:31 -0400

    Add working 16x16 grid

--- script/script.js
+++ script/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-  for (var i = 0; i < 16; i++) {
-    $("#grid").append("<div class='cell'></div>");
+  var grid = $("#grid");
+  var cellWidth = Math.floor($("#grid").width() / 16) - 2;
+  for (var i = 0; i < 16 * 16; i++) {
+    grid.append("<div class='cell'></div>");
   }
+  $(".cell").css({"width": cellWidth, "height": cellWidth, "margin-top": "-2px"});
+  
+  $(".cell").on("mouseenter", function() {
+    $(this).css("background-color", "red");
+  });
 });
