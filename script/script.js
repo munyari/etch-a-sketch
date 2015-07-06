dc18b78a3c180893557b6a463e103dcf41aa3a88
Panashe Fundira <fundirap@gmail.com>
2015-07-05 23:12:42 -0400

    Fix drawGrid function, add border rules to CSS

--- script/script.js
+++ script/script.js
@@ -2,15 +2,19 @@ var grid = $('#grid');
 var GRIDWIDTH = 960;
 
 // function that draws given response
-function() drawGrid(n) {
+function drawGrid(n) {
     var cellWidth = Math.floor(GRIDWIDTH / n) - 2;
-    for (var i = 0; i < response * response; i++) {
-        grid.append("div class='cell'></div>");
+    for (var i = 0; i < n * n; i++) {
+        grid.append("<div class='cell'></div>");
     }
+    $(".cell").css({height : cellWidth, width: cellWidth});
+
 }
 
 $(document).ready(function() {
     var response = +prompt("How many columns / rows do you want in your grid?");
+    drawGrid(response);
+
     $('button').on('click', function() {
         response = +prompt("How many columns / rows do you want in your grid?");
         drawGrid(response);
@@ -18,5 +22,6 @@ $(document).ready(function() {
     $(".cell").on("mouseeenter", function() {
         $(this).addClass("highlight");
     });
+    
     
 });
