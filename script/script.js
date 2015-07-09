bf6520057279280609fe10a34c00741fcc51a5fa
Panashe Fundira <fundirap@gmail.com>
2015-07-08 21:46:02 -0400

    Fix grid alignment issues

--- script/script.js
+++ script/script.js
@@ -4,8 +4,11 @@ var GRIDWIDTH = 960;
 // function that draws given response
 function drawGrid(n) {
     var cellWidth = Math.floor(GRIDWIDTH / n) - 2;
-    for (var i = 0; i < n * n; i++) {
-        grid.append("<div class='cell'></div>");
+    for (var j = 0; j < n; j++) {
+        for (var i = 0; i < n; i++) {
+            grid.append("<div class='cell'></div>");
+        }
+        grid.append($("<div></div>").css("clear", "both").height("1 px"));
     }
     $(".cell").css({height : cellWidth, width: cellWidth});
 
